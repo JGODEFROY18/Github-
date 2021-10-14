d2b1fc54d23bd3756d8260b042dbb2e00f0ed3ae
JGODEFROY18 <90181985+JGODEFROY18@users.noreply.github.com>
2021-10-14 16:18:23 +0200

    Update Cap2tp3Exo4.cpp

--- Cap2tp3Exo4.cpp
+++ Cap2tp3Exo4.cpp
@@ -4,12 +4,13 @@ int main()
 	int i; // On crée une variable i que l'on initiera a 0 par la suite.
 
 	printf("Entrez une chaine de caractere en majuscule : \n"); // On demande a l'utilisateur de rentrer une chaine de caractere en majuscule. 
-	gets_s(str); // 
-	for (i = 0; str[i] != '\0'; i++) {
-		if (str[i] >= 'A' && str[i] <= 'Z') {
-			str[i] = str[i] + 32;
+	gets_s(str); //Le gets nous permet de lire les caractere entrez par l'utilisateur.
+	for (i = 0; str[i] != '\0'; i++) { // On utilise donc par la suite suite une boucle For(pour). Qui dit que tant que i = 0 que str[i] n'est pas encore arriver au caractere \0 donc la fin de la chaine de caractere, on incremente de i + 1.
+		
+		if (str[i] >= 'A' && str[i] <= 'Z') { // Ensuite on dit que : Si str[i] est surperieur ou égal a A (Table ASCI) et que str[i] est inferieur a Z (table ASCI)
+			str[i] = str[i] + 32; 
 		}
 	}
-	printf("La chaine en miniscule= %s", str);
+	printf("La chaine en miniscule= %s", str); // %s est egal a la variable char et donc %s  a la chaine de caractere.
 	return 0;
 }
