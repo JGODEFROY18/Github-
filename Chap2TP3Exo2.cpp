0def15f3235a2c3e1e672d8e4adef240d05a60f7
JGODEFROY18 <90181985+JGODEFROY18@users.noreply.github.com>
2021-10-14 16:02:52 +0200

    Create Chap2TP3Exo2.cpp

--- Chap2TP3Exo2.cpp
+++ Chap2TP3Exo2.cpp
@@ -6,14 +6,14 @@
 
 int main()
 {
-	char s[100];
-	char *p;
+	char s[100]; // On incrémente nos variables,il est donc possible de rentrer une chaine de caractére de 100 caractére.
+	char p;
 	char c;
-	printf("Entrez une chaine de caractere :\n");
-	gets_s(s);
-	printf("Entrez un caractére: \n");
-	scanf("%c", &c);
-	p = strchr(s, c);
+	printf("Entrez une chaine de caractere :\n"); // On demande a l'utilisateur d'entrer la chaine de caractere.
+	gets_s(s); // Le gets permet de lire la réponse. 
+	printf("Entrez un caractére: \n"); // On demande a l'utilisateur d'entrer un seul caractere.
+	scanf("%c", &c); // Comme c'est un une chaine de caractere et une variable char, pour lire la réponse on utilise le %c.
+	p = strchr(s, c); // La fonction recherche strchr nous permet d'avoir la première occurrence du caractère passé en second paramètre dans la chaîne de caractères.
 
 	printf("Le caractere %c se trouve a la position %d à partir de la droite", c, p - s + 1);
 
