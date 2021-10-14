ce7e460ff65ff40f28814b2847b0df0c0cb10227
JGODEFROY18 <90181985+JGODEFROY18@users.noreply.github.com>
2021-10-14 16:08:57 +0200

    Update Cap2tp3Exo4.cpp

--- Cap2tp3Exo4.cpp
+++ Cap2tp3Exo4.cpp
@@ -1,10 +1,10 @@
 int main()
 {
-	char str[100];
-	int i;
+	char str[100]; // On incremente une chaine de caractere de 100 caracter a l'aide de la variable char.
+	int i; // On crée une variable i que l'on initiera a 0 par la suite.
 
-	printf("Voici la chaine a convertir en minuscules : \n");
-	gets_s(str);
+	printf("Entrez une chaine de caractere en majuscule : \n"); // On demande a l'utilisateur de rentrer une chaine de caractere en majuscule. 
+	gets_s(str); // 
 	for (i = 0; str[i] != '\0'; i++) {
 		if (str[i] >= 'A' && str[i] <= 'Z') {
 			str[i] = str[i] + 32;
