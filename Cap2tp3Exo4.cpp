43c5010b1456e3e3ee49e8f896c0d2e673662b28
JGODEFROY18 <90181985+JGODEFROY18@users.noreply.github.com>
2021-10-14 15:29:19 +0200

    Add files via upload

---
+++ Cap2tp3Exo4.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+	char str[100];
+	int i;
+
+	printf("Voici la chaine a convertir en minuscules : \n");
+	gets_s(str);
+	for (i = 0; str[i] != '\0'; i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			str[i] = str[i] + 32;
+		}
+	}
+	printf("La chaine en miniscule= %s", str);
+	return 0;
+}
